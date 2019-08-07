084ed736b5d8303b1cbeab6c97a83854c7c026f3
kwm42 <kwm2596123568@gmail.com>
2019-08-07 15:57:27 +0800

    modify build configs

---
+++ docs/precache-manifest.77eb684bbff1d9f50df767b37842855e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8076e9648992245e976505297c51d446",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef24896c98e7fd702241",
+    "url": "./static/css/2.c37f0f82.chunk.css"
+  },
+  {
+    "revision": "5ed238a3ccc9f30b2bd2",
+    "url": "./static/css/main.610a51bc.chunk.css"
+  },
+  {
+    "revision": "ef24896c98e7fd702241",
+    "url": "./static/js/2.bed902cc.chunk.js"
+  },
+  {
+    "revision": "5ed238a3ccc9f30b2bd2",
+    "url": "./static/js/main.172eef91.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
